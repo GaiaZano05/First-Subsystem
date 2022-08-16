905ba62bba761efd10faf76a83a2dc88bc7e7b14
Emma03 <emmalivne.03@gmail.com>
2022-08-16 18:50:20 +0300

    add conveyor command

---
+++ src/main/java/frc/robot/subsystems/conveyor/Convey.java
@@ -0,0 +1,28 @@
+package frc.robot.subsystems.conveyor;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.conveyor.Conveyor;
+
+public class Convey extends CommandBase {
+    private final Conveyor conveyor;
+    private final double power;
+
+    public Convey(Conveyor conveyor, double power){
+        this.conveyor=conveyor;
+        this.power=power;
+        addRequirements(conveyor);
+
+    }
+
+
+
+    @Override
+    public void execute() {
+        conveyor.setPower(power);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        conveyor.setPower(0);
+    }
+}
