b5755c472462b918c586b21b38bc3bc9f6eb49c3
Emma03 <emmalivne.03@gmail.com>
2022-08-16 18:51:32 +0300

    add intake

---
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -0,0 +1,45 @@
+package frc.robot.subsystems.intake;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.Ports;
+
+public class Intake extends SubsystemBase {
+    private static WPI_TalonFX motor = new WPI_TalonFX(Ports.INTAKE);
+    private static Solenoid piston = new Solenoid(PneumaticsModuleType.CTREPCM, Ports.PISTON);
+    private static Intake INSTANCE = null;
+
+    private Intake() {
+        motor.enableVoltageCompensation(Constants.ENABLE_VOLTAGE);
+        motor.setInverted(Ports.INVERTED);
+        motor.configVoltageCompSaturation(Constants.COMP_VOLTAGE);
+    }
+
+    public static double getPower() {
+        return motor.get();
+    }
+
+    public static void setPower(double power) {
+        motor.set(power);
+    }
+
+    public static Intake INSTANCE() {
+        if (INSTANCE == null) {
+            INSTANCE = new Intake();
+        }
+        return INSTANCE;
+    }
+
+    public static void toggle(){
+        piston.toggle();
+    }
+    public static void openPiston(){
+        piston.set(true);
+    }
+    public static void closePiston(){
+        piston.set(false);
+    }
+}
