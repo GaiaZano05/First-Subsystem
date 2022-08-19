5e70db5df92ebea1a7b8efbca8f7eab953d262fd
Jonathan Lotan <jonathan.lotan@gmail.com>
2022-08-19 10:58:47 +0300

    Create ports- motor, piston, inverted

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -1,6 +1,12 @@
 package frc.robot;
 
-public class Ports {
+import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
 
+public class Ports {
+public static int MOTOR = 0;
+public static int PISTON = 0;
+public static TalonFXInvertType INV = TalonFXInvertType.Clockwise;
 
 }
