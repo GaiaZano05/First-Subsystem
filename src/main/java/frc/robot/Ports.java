db8ac4b22609e3afa094d5d20bc5c07e361f92a1
Jonathan Lotan <jonathan.lotan@gmail.com>
2022-08-19 11:49:07 +0300

    Create motor

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -5,8 +5,13 @@ import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import edu.wpi.first.wpilibj.motorcontrol.Talon;
 
 public class Ports {
-public static int MOTOR = 0;
-public static int PISTON = 0;
-public static TalonFXInvertType INV = TalonFXInvertType.Clockwise;
+    public static class Intake {
+        public static int MOTOR = 0;
+        public static int PISTON = 0;
+        public static TalonFXInvertType INV = TalonFXInvertType.Clockwise;
+    }
+    public static class Conveyor {
+        public static int MOTOR = 0;
+    }
 
 }
