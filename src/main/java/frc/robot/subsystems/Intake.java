e08a99d04b887c75d5304bf6586b5edb03f7a8c3
omer <omeryakoby@gmail.com>
2022-08-23 17:26:35 +0300

    create motor and piston

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -5,5 +5,13 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 
 public class Intake extends SubsystemBase {
+    WPI_TalonFX motor;
+    Solenoid piston;
+
+    public Intake(){
+        motor = new WPI_TalonFX(Ports.Intake.MOTOR);
+        motor.setInverted(Ports.Intake.IS_INVERTED);
+        piston = new Solenoid(PneumaticsModuleType.CTREPCM, Ports.Intake.SELENOID);
+    }
 
 }
