b0d5611d1161d151502944ba113de3d5f7cb43b8
omer <omeryakoby@gmail.com>
2022-08-23 18:12:41 +0300

    move intake

--- src/main/java/frc/robot/subsystems/commands/MoveIntake.java
+++ src/main/java/frc/robot/subsystems/commands/MoveIntake.java
@@ -1,4 +1,22 @@
 package frc.robot.subsystems.commands;
 
-public class MoveIntake {
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.Constants;
+import frc.robot.subsystems.Intake;
+
+public class MoveIntake extends InstantCommand {
+    Intake intake;
+    private boolean mode;
+
+    public MoveIntake(Intake intake, boolean mode){
+        this.intake = intake;
+        this.mode = mode;
+        addRequirements(intake);
+    }
+
+    @Override
+    public void initialize() {
+        intake.setPiston(mode);
+    }
+
 }
